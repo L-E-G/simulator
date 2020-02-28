61a2e20158c774b3d9aec5de0427408b4bb77eee
rscibelli <rscibelli@umass.edu>
2020-02-27 22:56:27 -0500

    update get, update set

--- src/getset/get.rs
+++ src/getset/get.rs
@@ -4,11 +4,11 @@ use std::io::*;
 
 fn main() {
     let addr = "54";
-    let ret: String = get(addr.to_string());
+    let ret: String = get(addr);
     println!("{}", ret);
 }
 
-fn get(addr: String) -> String{
+fn get(addr: &str) -> String{
 
     let filename = "mem.txt";
     // Open the file in read-only mode (ignoring errors).
@@ -19,7 +19,7 @@ fn get(addr: String) -> String{
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap(); // Ignore errors.
         let data: Vec<&str> = line.split(" ").collect();
-        if addr == data[1].to_string(){
+        if addr.to_string() == data[1].to_string(){
             return data[0].to_string();
         }
     }
--- src/getset/set.rs
+++ src/getset/set.rs
@@ -14,19 +14,18 @@ fn set(data: &str, addr: &str) {
     let filename = "mem.txt";
     let mut file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut file2 = File::create("mem2.txt").unwrap();
+    let mut file2 = File::create("mem2.txt").unwrap();  //Need to be able to read and write from the same file
     let mut buffer = LineWriter::new(file2);
 
     let mut i = 0;
     for (index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let val: Vec<&str> = line.split(" ").collect();
-        
+
         let naddr: u32 = FromStr::from_str(addr).unwrap();
         let nval: u32 = FromStr::from_str(val[1]).unwrap();
 
         if naddr < nval && i==0{
-            println!("Rob");
             buffer.write_all(format!("{} {}\n",data, addr).as_bytes());
             i = 1;
         }
