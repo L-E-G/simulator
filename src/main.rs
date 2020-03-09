72807afe6f94d3a5e6028dcfb9ec43bb963f5104
rscibelli <rscibelli@umass.edu>
2020-03-09 00:00:35 -0400

    set funtion broken

--- src/main.rs
+++ src/main.rs
@@ -113,11 +113,11 @@ impl Memory<u32, u32> for DRAM {
         let mut tag: u32 = (address >> 10) << 10;
         for (index, line) in reader.lines().enumerate(){
             let line = line.unwrap();
-            let data: Vec<&str> = line.split(" ").collect();
-            let data0 = data[0].parse::<u32>().unwrap();
-            let data1 = data[1].parse::<u32>().unwrap();
-            let data2 = data[2].parse::<u32>().unwrap();
-            if tag == data0 && data1 == 1{
+            let items_in_line: Vec<&str> = line.split(" ").collect();
+            let data0 = items_in_line[0].parse::<u32>().unwrap();
+            let data1 = items_in_line[1].parse::<u32>().unwrap();
+            let data2 = items_in_line[2].parse::<u32>().unwrap();
+            if address == data0 && data1 == 1{
                 return SimResult::Wait(self.delay, data2);
             }
         }
@@ -142,7 +142,7 @@ impl Memory<u32, u32> for DRAM {
             let data0 = items_in_line[0].parse::<u32>().unwrap();
             let data1 = items_in_line[1].parse::<u32>().unwrap();
             let data2 = items_in_line[2].parse::<u32>().unwrap();
-            if tag < data0 && data1 == 1 && i==0{
+            if address < data0 && data1 == 1 && i==0{
                 writer.write_all(format!("{} 1 {}\n",address.to_string(), data.to_string()).as_bytes());
                 i=1;
             }
